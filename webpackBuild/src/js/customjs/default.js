0f1d3a49edd8dd54765787147a1c6aeb17baa596
frontenedalex <alex.lych@outlook.com>
2022-05-03 20:06:13 +0300

    public cleanup

--- webpackBuild/src/js/customjs/default.js
+++
@@ -1,9 +0,0 @@
-import { setActiveMenu } from './activeMenu';
-import headerclick from './headerclick'
-
-function initCustomScripts () {
-    headerclick();
-    setActiveMenu();
-}
-
-export default initCustomScripts
